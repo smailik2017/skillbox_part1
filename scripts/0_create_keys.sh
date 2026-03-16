8dc2234fc1dcbf6de5f4e50cb85a52aa20be5f68
Maks <mvfedorovskiy@gmail.com>
2026-03-16 12:39:36 +0000

    add request email and keyname for generating ssh keypair during start script 0_create_keys.sh

--- scripts/0_create_keys.sh
+++ scripts/0_create_keys.sh
@@ -5,7 +5,14 @@
 #
 ##
 
-ssh-keygen -t ed25519 -C "smailik@example.com" -f ~/.ssh/timeweb
+read -r -p "Enter your email addres (default: test@example.com): " EMAIL 
+read -r -p "key name (default: test-infra): " KEYNAME 
+[ -z "$EMAIL" ] && EMAIL="test@example.com"
+[ -z "$KEYNAME" ] && KEYNAME="test-infra"
+
+echo "Generating key pair with email: $EMAIL and name: $KEYNAME"
+
+ssh-keygen -t ed25519 -C "$EMAIL" -f "$HOME/.ssh/$KEYNAME"
 
 cd - || exit 1
 
--- scripts/debs-install/1_inst_mgmt.sh
+++ scripts/debs-install/1_inst_mgmt.sh
@@ -2,8 +2,13 @@
 ##
 #
 # Script install MGMT server
+# $1 - private key to manage all servere
+# EXAMPLE: ./1_inst_mgmt.sh ~/.ssh/timeweb
 #
 ##
+
+# copy private to mgmt server
+scp "$1" my-mgmt-ext:~/.ssh/
 
 # start install
 cd "$(dirname "$0")" || exit 1
